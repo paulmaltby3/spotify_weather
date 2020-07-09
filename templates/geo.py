1becfbe3efebf568c77833a7c93b1378b7bc6d06
Paul Maltby <paul.maltby3@gmail.com>
2020-07-09 21:43:32 +0100

    deleted other stuff not needed

--- templates/geo.py
+++
@@ -1,9 +0,0 @@
-from geopy.geocoders import Nominatim
-geolocator = Nominatim(user_agent="")
-location = geolocator.geocode("19 ")
-print(location.address)
-
-lat = location.latitude
-lon = location.longitude
-
-print((location.latitude, location.longitude)) 
